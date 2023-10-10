d43eed242f7777d08067e5c93f4f7539d283c1ea
Robin Fantino <robin.fantino.rf@gmail.com>
2023-10-10 10:46:15 +0200

    add some dynamics for pokemon card if img src is presents

--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,9 +1,23 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0]
+    const styleName = { fontSize: '3em', backgroundColor: 'lightGreen' }
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
+        <>
+            {pokemon.imgSrc ?
+                <img src={pokemon.imgSrc} alt={pokemon.name} style={styleName} /> : <p>???</p>}
+            <figcaption style={styleName}>{pokemon.name}</figcaption>
+        </>
     )
 }
 export default PokemonCard;
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
